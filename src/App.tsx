e0775f5fadd31520542bd6634ad6d946fae0e94f
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2020-10-04 17:33:22 +0300

    feat: use navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { ThemeProvider } from 'emotion-theming';
 import theme from './theme';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/molecules/Navbar';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Router>        
-        Test
+      <Router>
+        <Navbar />
+        <div>
+          <p>Similique in culpa libero sagittis lacinia gravida suscipit nostrud adipiscing.</p>
+          <p>Proin porta voluptates qui porta harum cumque itaque, exercitation? Asperiores.</p>
+          <p>Beatae necessitatibus lobortis taciti eaque eius! Incidunt vivamus voluptates, tristique.</p>
+        </div>
       </Router>
     </ThemeProvider>
   );
